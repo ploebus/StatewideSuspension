c07281b2a7734e356bf91a60a56aad13ecacf6ae
John Garvey <johng@urbanstrategies.org>
2012-05-29 15:01:52 -0700

    third commit

---
+++ js/johnUtils.js
@@ -0,0 +1,18 @@
+ function StringtoXML(text){
+                if (window.ActiveXObject){
+                  var doc=new ActiveXObject('Microsoft.XMLDOM');
+                  doc.async='false';
+                  doc.loadXML(text);
+                } else {
+                  var parser=new DOMParser();
+                  var doc=parser.parseFromString(text,'text/xml');
+                }
+                return doc;
+            }
+            
+
+function getLegendItem(low, high, color){
+		
+}
+
+
